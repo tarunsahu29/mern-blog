252803e696d9ba08283f8a96a25d2e1a54569029
Tarun Sahu <tarun929s@192.168.1.12>
2024-02-04 01:11:10 +0530

    complete user image upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,29 +1,35 @@
-import { Sidebar } from "flowbite-react"
-import { useEffect, useState } from "react";
-import { HiArrowSmRight, HiUser } from 'react-icons/hi';
-import { Link, useLocation } from "react-router-dom";
+import { Sidebar } from 'flowbite-react'
+import { useEffect, useState } from 'react'
+import { HiArrowSmRight, HiUser } from 'react-icons/hi'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function DashSidebar() {
-  const location = useLocation();
+  const location = useLocation()
   // eslint-disable-next-line no-unused-vars
-  const [tab, setTab] = useState('');
+  const [tab, setTab] = useState('')
   useEffect(() => {
-    const urlParams = new URLSearchParams(location.search);
-    const tabFromUrl = urlParams.get('tab');
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
     if (tabFromUrl) {
-      setTab(tabFromUrl);
+      setTab(tabFromUrl)
     }
-  },[location.search])
+  }, [location.search])
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Link to='/dashboard?tab=profile'>
-          <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor = 'dark'>
-            Profile
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === 'profile'}
+              icon={HiUser}
+              label={'User'}
+              labelColor="dark"
+              as = 'div'
+            >
+              Profile
             </Sidebar.Item>
-            </Link>
-          <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
+          </Link>
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
             Sign out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
