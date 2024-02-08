3acfae9308d754560cb4f9c7c34773810428354a
Tarun Sahu <tarun929s@192.168.1.4>
2024-02-08 19:41:40 +0530

    show users to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -5,6 +5,7 @@ import {
   HiArrowSmRight,
   HiDocumentText,
   HiOutlineExclamationCircle,
+  HiOutlineUserGroup,
   HiUser,
 } from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom'
@@ -66,6 +67,17 @@ export default function DashSidebar() {
                 as="div"
               >
                 Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+          {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=users">
+              <Sidebar.Item
+                active={tab === 'users'}
+                icon={HiOutlineUserGroup}
+                as="div"
+              >
+                Users
               </Sidebar.Item>
             </Link>
           )}
