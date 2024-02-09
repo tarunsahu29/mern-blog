8240de0f68b95cb96d880e0f5cda86142d4d1239
Tarun Sahu <tarun929s@192.168.1.4>
2024-02-09 16:18:05 +0530

    add comment section to the post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,10 +13,12 @@ import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePost'
 import PostPage from './pages/PostPage'
+import ScrollToTop from './components/ScrollToTop'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
@@ -24,12 +26,12 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
 
-        <Route element={<PrivateRoute />}>\
-          <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route element={<PrivateRoute />}>
+          \<Route path="/dashboard" element={<Dashboard />}></Route>
         </Route>
 
-        <Route element={<OnlyAdminPrivateRoute />}>\
-          <Route path="/create-post" element={<CreatePost />}></Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          \<Route path="/create-post" element={<CreatePost />}></Route>
           <Route path="/update-post/:postId" element={<UpdatePost />}></Route>
         </Route>
 
