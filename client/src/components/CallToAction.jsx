57c3e27d21ef4babbf41d4ec46cb17344d5ba6dc
Tarun Sahu <tarun929s@192.168.1.19>
2024-02-12 23:34:39 +0530

    complete home page

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -4,13 +4,16 @@ export default function CallToAction() {
   return (
     <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
       <div className="flex-1 justify-center flex flex-col">
-        <h2 className="text-2xl">Excited about Artificial Intelligence?</h2>
+        <h2 className="text-2xl">
+          Excited about Artificial Intelligence? Dive into the fascinating world
+          of AI!
+        </h2>
         <p className="text-gray-500 my-2">
           Explore the possibilities with our comprehensive AI guide.
         </p>
         <Button
           gradientDuoTone="purpleToPink"
-          className="rounded-tl-none rounded-br-xl"
+          className="rounded-tl-xl rounded-bl-none"
         >
           <a
             href="https://www.mckinsey.com/capabilities/quantumblack/our-insights/an-executives-guide-to-ai"
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,63 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Link } from 'react-router-dom'
+import CallToAction from '../components/CallToAction'
+import { useEffect, useState } from 'react'
+import PostCard from '../components/PostCard'
 
 export default function Home() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/post/getPosts')
+      const data = await res.json()
+      setPosts(data.posts)
+    }
+    fetchPosts()
+  }, [])
   return (
-    <div>Home</div>
+    <div>
+      <div className="flex flex-col gap-6 p-24 px-3 max-w-6xl mx-auto">
+        <h1 className="text-xl font-bold lg:text-5xl">
+          Embark on a Journey Through Ideas!
+        </h1>
+        <p className="text-gray-500 text-xs sm:text-sm dark:text-slate-400">
+          Welcome to a world of inspiration! Discover captivating blogs spanning
+          lifestyle, finance, sports, technology, nature, and beyond. Immerse
+          yourself in a diverse array of perspectives, where creativity knows no
+          bounds. While you're here, feel free to explore and enjoy the wealth
+          of knowledge our blogosphere has to offer. Remember, in this space,
+          the adventure of discovery awaits you!
+        </p>
+        <Link
+          to="/search"
+          className="text-xs sm:text-sm text-teal-500 font-semi-bold hover:underline"
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className="p-2 dark:bg-slate-700 rounded-tl-3xl rounded-br-3xl bg-amber-50">
+        <CallToAction />
+      </div>
+
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+            <div className="flex flex-wrap gap-4">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={'/search'}
+              className="text-lg text-teal-500 hover:underline text-center"
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
