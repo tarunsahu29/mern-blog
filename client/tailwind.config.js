6e1b5e256019595732724933c4cef2bf43ef32b9
Tarun Sahu <tarun929s@Taruns-MacBook-Air.local>
2024-02-12 10:52:39 +0530

    add recent article section to the post page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,12 +2,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
     'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin'), require('tailwind-scrollbar')],
+  plugins: [
+    require('flowbite/plugin'),
+    require('tailwind-scrollbar'),
+  ],
 }
